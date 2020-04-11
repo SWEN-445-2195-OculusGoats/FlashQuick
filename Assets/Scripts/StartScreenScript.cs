9305ddabfbd0772608e3a9df074d9f26305ee811
sauloferreira <srf6413@g.rit.edu>
2020-04-10 20:42:33 -0400

    Issues #1 and #10 complete

---
+++ Assets/Scripts/StartScreenScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreenScript : MonoBehaviour
+{
+    void Update(){
+        if(Input.GetMouseButtonDown(0)){
+            RaycastHit hit = new RaycastHit();
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast (ray, out hit)){
+                if(hit.transform != null){
+                    PrintName(hit.transform.gameObject);
+                    SceneManager.LoadScene("QuizSelectScreen");
+                }
+            }
+        }
+    }
+
+    void PrintName(GameObject go){
+        print(go.name);
+    }
+}
