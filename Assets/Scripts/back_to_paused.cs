2b99cb851f479831e48fe8be875a4fbacfbd99c9
sflemingo <stf8586@g.rit.edu>
2020-04-12 15:09:46 -0400

    fixed back button on game options script

--- Assets/Scripts/back_to_paused.cs
+++ Assets/Scripts/back_to_paused.cs
@@ -14,10 +14,22 @@ public class back_to_paused : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            SceneManager.LoadScene("GamePausedScene");
-        }
-        
+        if (Input.GetMouseButtonDown(0)){
+            RaycastHit hit = new RaycastHit();
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast (ray, out hit)){
+                if(hit.transform != null){
+                    PrintName(hit.transform.gameObject);
+                    if (hit.transform.gameObject.name == "BackButton"){
+                        SceneManager.LoadScene("GamePausedScene");
+                    }
+                }  
+            }
+		}
+    }
+
+	void PrintName(GameObject go){
+        print(go.name);
     }
 }
